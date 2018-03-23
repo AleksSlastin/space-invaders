bc59cc8fd48de7302a68e029cf5cf9c61be6c4ec
aleks <aleks.slastin@rambler.ru>
2018-03-23 16:58:06 +0200

    'space_invaders'

--- space_invaders.py
+++ space_invaders.py
@@ -17,11 +17,15 @@ def run_game():
     ship = Ship(si_settings, screen)
 
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(si_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(si_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(si_settings, screen, ship, bullets)
+        gf.update_aliens(si_settings, aliens)
+        gf.update_screen(si_settings, screen, ship, aliens, bullets)
 
 run_game()
